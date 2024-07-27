b29ed3125109a63a065f8dddc70d040a6cc9c078
Namki Kwon <75561206+namgi1234@users.noreply.github.com>
2024-07-28 00:44:34 +0900

    Update webcam.py

--- porthole/Yolov8/webcam.py
+++ porthole/Yolov8/webcam.py
@@ -21,7 +21,7 @@ cap.set(4, 480)
 model = YOLO("best.pt")
 
 # object classes
-classNames = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+classNames = ["pothole"]
 confidence_data = []
 
 while True:
